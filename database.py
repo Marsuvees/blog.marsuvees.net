1707e45b2a6b01528dc1f5cb0ca6da803b2f8d78
marsuvees <tolujed@gmail.com>
2023-11-27 06:39:21 +0100

    added the welcome post to the database

--- database.py
+++ database.py
@@ -17,10 +17,19 @@ class User(Base):
     date_of_birth = Column(DateTime, nullable=True)
     password = Column(String, nullable=False)
 
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(length=100), nullable=False)
+    content = Column(String, nullable=False)
+    post_datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
+    author_id = Column(Integer, ForeignKey("users.id"))
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-    admin = User(username="admin", password=bcrypt.hashpw("admin".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"))
-    session.add(admin)
+    # admin = User(username="admin", password=bcrypt.hashpw("admin".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"))
+    welcome_post = Post(title="Welcome to blog.marsuvees", content="This is the default welcome post", author_id=1, post_datetime= datetime.now())
+    session.add(welcome_post)
     session.commit()
 
 
