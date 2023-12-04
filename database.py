f992392f2d48d9c5db66d1b95801d5756832442c
marsuvees <tolujed@gmail.com>
2023-12-04 07:59:19 +0100

    added blog-tag association

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Table
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from datetime import datetime
 import bcrypt
@@ -29,7 +29,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     post_datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
     author_id = Column(Integer, ForeignKey("users.id"))
-    tags = relationship("Tag")
+    tags = relationship("Tag", secondary="blog_tag_association", back_populates="posts")
 
     def __repr__(self):
         return f"<Post(title={self.title})>"
@@ -39,25 +39,35 @@ class Comment(Base):
     id = Column(Integer, primary_key=True)
     content = Column(String, nullable=False)
     post_datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
-    author_id = Column(Integer, ForeignKey("users.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
+    author_id = Column(ForeignKey("users.id"))
+    post_id = Column(ForeignKey("posts.id"))
 
 class Tag(Base):
     __tablename__ = "tags"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    posts = relationship("Post")
+    posts = relationship("Post", secondary="blog_tag_association", back_populates="tags")
 
     def __repr__(self):
         return f"<Tag(tag = {self.name})>"
 
+blog_post_tag_association = Table(
+    'blog_tag_association',
+    Base.metadata,
+    Column('blog_post_id', ForeignKey('posts.id'), primary_key=True),
+    Column('tag_id', ForeignKey('tags.id'), primary_key=True)
+)
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     admin = User(username="admin", password=bcrypt.hashpw("admin".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"))
-    welcome_tag = Tag(name = "welcome")
-    welcome_post = Post(title="Welcome to blog.marsuvees", content="This is the default welcome post", author_id=1, post_datetime= datetime.now(), tag="welcome")
+    session.add(admin)
+    tag1 = Tag(name="welcome")
+    tag2 = Tag(name="first")
+    session.add_all((tag1, tag2))
+    welcome_post = Post(title="Welcome to blog.marsuvees", content="This is the default welcome post", author_id=1, post_datetime= datetime.now(), tags=[tag1, tag2]) 
     test_comment = Comment(content="this is a test of the comments.", author_id=1, post_id=1)
-    session.add(test_comment)
+    session.add(welcome_post)
     session.commit()
 
 
